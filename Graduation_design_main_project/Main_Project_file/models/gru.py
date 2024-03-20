621cf445bb533a882b4005e9dc62f6ea67de2c55
程骏 <chengjun.miracle@gmail.com>
2024-03-20 12:24:50 +0800

    开始搭建一个真正的项目文件，包括utils,models等

---
+++ Graduation_design_main_project/Main_Project_file/models/gru.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import time
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+sys.path.append(ROOT_DIR)
+
+from utils.time import time_calc
+
+
