5c334cd0744ebb08c08a65b718084cacfa96a678
程骏 <chengjun.miracle@gmail.com>
2024-03-20 10:36:57 +0800

    提交卷积循环神经网络测试代码（未进行归类）目前卷积神经网络测试效果不好，需要搞清楚原因；

--- Graduation_design_main_project/models.py
+++ Graduation_design_main_project/models.py
@@ -1,72 +1,99 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import random
+import numpy as np
+import torch
+import torch.optim as optim
 
 
 class WaveNet(nn.Module):
-    def __init__(self, cnn_channels, rnn_hidden_size, rnn_layers, num_classes):
+    def __init__(self, cnn_channels, rnn_hidden_size, rnn_layers):
         super(WaveNet, self).__init__()
         self.cnn_channels = cnn_channels
-        self.rnn_hidden_size = rnn_hidden_size
+        self.rnn_hidden_size = rnn_hidden_size # 252
         self.rnn_layers = rnn_layers
-        self.num_classes = num_classes
 
         # CNN layers
-        self.conv1 = nn.Conv1d(in_channels=1, out_channels=cnn_channels, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv1d(in_channels=cnn_channels, out_channels=cnn_channels, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=cnn_channels, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=cnn_channels, out_channels=cnn_channels, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=cnn_channels, out_channels=1, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.flatten = nn.Flatten()
 
         # RNN layers
-        self.rnn = nn.GRU(input_size=cnn_channels, hidden_size=rnn_hidden_size, num_layers=rnn_layers, batch_first=True)
+        self.rnn = nn.GRU(input_size=57600, hidden_size=rnn_hidden_size, num_layers=rnn_layers, batch_first=True)
 
-        # CNN layers after RNN
-        self.conv3 = nn.Conv1d(in_channels=rnn_hidden_size, out_channels=cnn_channels, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv1d(in_channels=cnn_channels, out_channels=cnn_channels, kernel_size=3, padding=1)
+        # # CNN layers after RNN
+        # self.conv3 = nn.Conv1d(in_channels=rnn_hidden_size, out_channels=cnn_channels, kernel_size=3, padding=1)
+        # self.conv4 = nn.Conv1d(in_channels=cnn_channels, out_channels=cnn_channels, kernel_size=3, padding=1)
 
         # Output layer
-        self.fc = nn.Linear(in_features=cnn_channels, out_features=num_classes)
+        self.fc = nn.Linear(in_features=rnn_hidden_size, out_features=230400)
 
     def forward(self, x):
         # CNN layers
         x = F.relu(self.conv1(x))
-        x = self.pool(F.relu(self.conv2(x)))
+        x = F.relu(self.conv2(x))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.flatten(x)
+        print(x.size())
 
         # Reshape for RNN
-        x = x.permute(0, 2,
-                      1)  # Change shape from [batch_size, channels, sequence_length] to [batch_size, sequence_length, channels]
+        x = x.unsqueeze(0) # Change shape from [batch_size, channels, sequence_length] to [batch_size, sequence_length, channels]
 
         # RNN layers
         x, _ = self.rnn(x)
 
-        # CNN layers after RNN
-        x = F.relu(self.conv3(x.permute(0, 2, 1)))  # Change shape back to [batch_size, channels, sequence_length]
-        x = self.pool(F.relu(self.conv4(x)))
-
-        # Global average pooling
-        x = F.avg_pool1d(x, kernel_size=x.shape[2])
-        x = x.view(-1, self.cnn_channels)
 
         # Output layer
         x = self.fc(x)
+
+        x = x.view(80,720,320)
         return x
+def seq_data_iter_random(corpus, batch_size, num_steps):
+    """使用随机抽样生成一个小批量子序列
+
+    Defined in :numref:`sec_language_model`"""
+    # 从随机偏移量开始对序列进行分区，随机范围包括num_steps-1
+    corpus = corpus[random.randint(0, num_steps - 1):]
+    # 减去1，是因为我们需要考虑标签
+    num_subseqs = (len(corpus) - 30) // num_steps
+    # 长度为num_steps的子序列的起始索引
+    initial_indices = list(range(0, num_subseqs * num_steps, num_steps))
+    # 在随机抽样的迭代过程中，
+    # 来自两个相邻的、随机的、小批量中的子序列不一定在原始序列上相邻
+    random.shuffle(initial_indices)
+    def data(pos):
+        # 返回从pos位置开始的长度为num_steps的序列
+        return corpus[pos: pos + num_steps]
+
+    num_batches = num_subseqs // batch_size
+    for i in range(0, batch_size * num_batches, batch_size):
+        # 在这里，initial_indices包含子序列的随机起始索引
+        initial_indices_per_batch = initial_indices[i: i + batch_size]
+        X = [data(j) for j in initial_indices_per_batch]
+        Y = [data(j + 30) for j in initial_indices_per_batch]
+        yield torch.tensor(X), torch.tensor(Y)
+
+# 读取.npy文件
+eta00 = np.load('data/1000_1s_data_eta00.npy')
+model = WaveNet(64, 256, 1).to('cuda')
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), 0.01)
 
 
-# Example usage:
-# Define model parameters
-cnn_channels = 64
-rnn_hidden_size = 128
-rnn_layers = 2
-num_classes = 10
+for X, Y in seq_data_iter_random(eta00, batch_size=1, num_steps=80):
+    X = X.permute(1, 0, 2, 3).float().to('cuda')
+    output = model(X)
+    # output, hidden_prev = model(X, hidden_prev)
+    # hidden_prev = hidden_prev.detach()
+    Y = Y.squeeze().float().to('cuda')
+    loss = criterion(output, Y)
+    model.zero_grad()
+    loss.backward()
 
-# Create model instance
-model = WaveNet(64, 128, 2, 10)
+    optimizer.step()
 
-# Example input
-batch_size = 32
-sequence_length = 100
-input_channels = 1
-input_data = torch.randn(32,1,)
 
-# Forward pass
-output = model(input_data)
-print("Output shape:", output.shape)
+    print("Iteration:{} loss {}".format(iter, loss.item()))
