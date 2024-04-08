7fd5c5776ae4d9a38d36e8b912b6c94a0121376a
程骏 <chengjun.miracle@gmail.com>
2024-04-08 16:51:57 +0800

    优化算法（改一改BUG）

--- train_RNN.py
+++ train_RNN.py
@@ -18,11 +18,11 @@ for epoch in range(200):
     model.train()
     train_loss = 0
     
-    for i in range(4000):
-        X = data[0+i:30+i]
+    for i in range(800):
+        X = data[i*60:i*60+30]
 
 
-        Y = data[30+i:60+i]
+        Y = data[i*60+30:i*60+60]
         X = np.expand_dims(X, axis=0)
         Y = np.expand_dims(Y, axis=0)
 
@@ -43,7 +43,7 @@ for epoch in range(200):
 
         optimizer.step()
 
-    train_loss =  train_loss / 4000
+    train_loss =  train_loss / 800
 
     print(f'Epoch [{epoch+1}/200], train Loss: {train_loss}')
 
@@ -51,11 +51,11 @@ for epoch in range(200):
     if epoch % 10 == 0:
         model.eval() 
         test_loss = 0
-        for i in range(1500):
-            X = data[4060+i:4090+i]
+        for i in range(200):
+            X = data[48000 + (i*60):48000 + (i*60) +30]
 
 
-            Y = data[4090+i:4120+i]
+            Y = data[48000 + (i*60) +30:48000 + (i*60) +60]
             X = np.expand_dims(X, axis=0)
             Y = np.expand_dims(Y, axis=0)
 
@@ -67,7 +67,7 @@ for epoch in range(200):
             loss = loss_fn(output, Y_tensor)
 
             test_loss += loss.item()
-        test_loss = test_loss / 1500
+        test_loss = test_loss / 200
 
         print('-------------------------------------------')
         print(f'Epoch [{epoch+1}/200], test Loss: {test_loss}')
