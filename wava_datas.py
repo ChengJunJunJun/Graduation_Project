1f3e8c78c2045399c79d3ef41740bcfba8121aa1
程骏 <chengjun.miracle@gmail.com>
2024-04-12 13:27:30 +0800

    优化程序,生成数据

--- wava_datas.py
+++ wava_datas.py
@@ -1,76 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
-from scipy.fft import fftn, fftfreq, ifftn, fftshift, ifftshift
+import math
+import random
 
+def range_omega(H0, T0, start_omega, end_omega):
+    """
+    get range of omega
+    :param H0:
+    :param T0:
+    :return:
+    """
+    w = np.linspace(start_omega, end_omega, 100)
+    sw = 173 * H0 ** 2 * T0 ** (-4) * w ** (-5) * np.exp(-691 * w ** (-4) * T0 ** (-4))
+    # sw = (1 / 4 * math.pi) * (H0 ** 2) * (((2 * math.pi)/T0) ** 4) * w ** (-5) * np.exp((-1 / math.pi) * (((2 * math.pi)/T0) ** 4) * w ** (-4))
+    plt.plot(w, sw)
+    plt.show()
 
-# 时间采样帧数、采样间隔
-T = 60 #(自己根据需要设置)
-delta_t = 1
-# 频率划分份数
-N_omg = 30
-# 角度划分份数
-N_theta = 30
-# 频率间隔
-start_omega = 0.45
-end_omega = 1.5
-delta_omega0 = (end_omega-start_omega)/N_omg
-# 角度间隔
-start_theta = -np.pi/2
-end_theta = np.pi/2
-delta_theta = (end_theta-start_theta)/N_theta
-# 重力加速度
+def s(w,T0,H0):
+    sw = 173 * H0 ** 2 * T0 ** (-4) * w ** (-5) * np.exp(-691 * w ** (-4) * T0 ** (-4))
+    return sw
+
+T = 20000
+H0 = 4
+T0 = 3.5
+Tz = T0 / 1.408
+start_omega = 0.8
+end_omega = 4
 g = 9.8
-# 径向分辨率、径向范围、径向采样数目
-r_delta = 10
-r_end = 1000
-r_num = int(r_end/r_delta)
+# # start_omega = (-3.11/((H0 ** 2) * math.log(10, mu))) ** (1/4)
+# # end_omega = (-3.11/((H0 ** 2) * math.log(10, (1 - mu)))) ** (1/4)
+
+# start_omega = (1 / T0) * (-1605.3/math.log(10, mu)) ** (1/4)
+# end_omega = (1 / T0) * (-1605.3/math.log(10, (1 - mu))) ** (1/4)
 
 
-def gennerate_data():
-    # 随机频率、传播角、相位角
-    omega_ran = np.linspace(start_omega, end_omega, N_omg)
-    theta_ran = np.linspace(start_theta, end_theta, N_theta)
-    epsilon_ran = np.linspace(0, 2*np.pi, N_omg*N_theta)
-    np.random.shuffle(epsilon_ran)
 
-    # 周期和有义波高
-    T0 = 3.5 #（根据需要设置）
-    H0 = 4 #（根据需要设置）
+# range_omega(H0, T0, start_omega, end_omega)
+eta00 = np.zeros([T, 100])
+z = np.zeros([100])
+M = 60
+delta_w = (end_omega - start_omega) / M
+w = np.linspace(start_omega, end_omega, M)
 
-    eta00 = np.zeros([T, r_num])
-    for n in range(T):
-        t = delta_t*(n+1)
-        z = np.zeros([r_num])
-        r = np.linspace(0, r_end, r_num)
-        num = 0
-        # 叠加波分量
-        for i in range(N_omg):
-            for j in range(N_theta):
-                omega_i = omega_ran[i]
-                k_i = omega_i**2/g
-                theta_j = theta_ran[j]
-                epsilon_ij = epsilon_ran[num]
-                num += 1
-                a_ij = np.sqrt(2*(173*H0**2*T0**(-4)*omega_i**(-5)*np.exp(-691*omega_i**(-4)*T0**(-4))*2/np.pi*(np.cos(theta_j))**2)*delta_omega0*delta_theta)
-                z += a_ij*np.cos(omega_i*t-k_i*r*np.cos(theta_j)+epsilon_ij)
-        eta00[n, :] = z
-    
-    return eta00
+for t in range(T):
+    for i in range(M-1):
+        x = np.linspace(1, 100, 100)
+        w_i = random.uniform(w[i], w[i + 1])
+        phi_i = random.uniform(0, 2 * math.pi)
+        k_i = w_i**2/g
+        a_i = (2 * delta_w * s(w_i, T0, H0)) ** 0.5
+        z += a_i * np.cos(k_i * x - w_i * t + phi_i)
+    eta00[t, :] = z
 
-combined_data = np.empty((0, 100))
-N_num = 1000
-for i in range(N_num):
-    data = gennerate_data()
+    print(f"已经完成{1 + t} / {T}个时间步")
 
-    combined_data = np.concatenate((combined_data,data), axis=0)
-
-    print(f"已经完成{1 + i} / {N_num}")
-
-
-print(combined_data.shape)
-
-
-np.save(r'D:/data/chengjun.npy', combined_data)
-
-print('原始波浪场仿真完毕')
+np.save(r'D:/data/chengjun.npy', eta00)
